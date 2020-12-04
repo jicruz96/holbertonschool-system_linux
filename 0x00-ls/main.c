68c9fdae562293a2a9b92901c1acaf72a0293cf7
jicruz96 <accounts@jicruz.com>
2020-12-04 07:15:59 +0000

    add helper function to free all linked lists and directory streams

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 {
 	int i, error_check = 0;
 	ls_config_t config = {NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	dir_node_t *dirs_list = NULL; /* See dir_node struct in header.h */
+	dir_node_t *dirs_list = NULL, *tmp;
 
 	/* Initialize config struct by including the program name in config */
 	config.program_name = argv[0];
@@ -30,21 +30,47 @@ int main(int argc, char **argv)
 	}
 	else /* else, print the directory name before and a newline after */
 	{
-		for (; dirs_list != NULL; dirs_list = dirs_list->next)
+		for (tmp = dirs_list; tmp != NULL; tmp = tmp->next)
 		{
-			if (dirs_list->error_code)
+			if (tmp->error_code)
 			{
-				print_error_message(dirs_list, argv[0]);
+				print_error_message(tmp, argv[0]);
 			}
 			else
 			{
-				printf("%s:\n", dirs_list->dir_name);
-				print_list(dirs_list->list, config);
+				printf("%s:\n", tmp->dir_name);
+				print_list(tmp->list, config);
 			}
 
-			if (dirs_list->next != NULL)
+			if (tmp->next != NULL)
 				putchar('\n');
 		}
 	}
+	free_everything(dirs_list);
 	return (error_check);
+}
+
+/**
+ * free_everything - yup
+ * @head: head
+ **/
+void free_everything(dir_node_t *head)
+{
+	file_node_t *f, *f_prev;
+	dir_node_t *prev;
+
+	while (head)
+	{
+		f = head->list;
+
+		while (f)
+		{
+			f_prev = f;
+			f = f->next;
+			free(f_prev);
+		}
+		prev = head;
+		head = head->next;
+		free(prev);
+	}
 }
