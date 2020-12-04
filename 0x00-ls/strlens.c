370632bbc3411daaeaa8a275a5ebf5ebf8cb10b6
jicruz96 <accounts@jicruz.com>
2020-12-04 06:51:36 +0000

    make comply with style recs

--- 0x00-ls/strlens.c
+++ 0x00-ls/strlens.c
@@ -1,13 +1,18 @@
 #include "header.h"
 
+/**
+ * _strlen - custom strlen
+ * @str: string
+ * Return: string length
+ **/
 int _strlen(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    if (str)
-    {
-        while (str[i] != '\0')
-            i++;
-    }
-    return (i);
+	if (str)
+	{
+		while (str[i] != '\0')
+			i++;
+	}
+	return (i);
 }
