51c9870af86d64750ddd8a1e74babd9e90b4dab2
jicruz96 <accounts@jicruz.com>
2020-12-06 20:22:38 +0000

    spruce up the header with some comments

--- 0x00-ls/header.h
+++ 0x00-ls/header.h
@@ -1,67 +1,31 @@
 #ifndef HLS_HEADER
 #define HLS_HEADER
 
+/** HEADERS **/
+
 /* Header for opendir, readdir, closedir, lstat */
 #include <sys/types.h>
-
 /* Header for opendir, readdir, closedir */
 #include <dirent.h>
-
 /* Header for printf, sprintf, fprintf */
 #include <stdio.h>
-
 /* Headers for lstat */
 #include <sys/stat.h>
 #include <unistd.h>
-
 /* Header for errno */
 #include <errno.h>
-
 /* Header for ctime */
 #include <time.h>
-
 /* For booleans */
 #include <stdbool.h>
-
 /* For getpwuid */
 #include <pwd.h>
-
 /* For getgrgid */
 #include <grp.h>
-
 /* For malloc, free */
 #include <stdlib.h>
 
-/**
- * struct ls_settings_s - flags struct
- * @long_format: long format
- * @one_per_line: -1
- * @dot: -a
- * @dot_alt: -A ('.' and '..' ommitted)
- * @reversed: -r
- * @sort_by_size: -S
- * @sort_by_time: -t
- * @recursive: -R
- * @print_dir_name: if true, print directory name before printing list
- * @max_hard_links: max hard links
- * @max_size: max size
- * @max_strlen: max strlen
- **/
-typedef struct ls_settings_s
-{
-	bool long_format;
-	bool one_per_line;
-	bool dot;
-	bool dot_alt;
-	bool reversed;
-	bool sort_by_size;
-	bool sort_by_time;
-	bool recursive;
-	bool print_dir_name;
-	int max_hard_links;
-	int max_size;
-	int max_strlen;
-} ls_config_t;
+/** STRUCTS **/
 
 /**
  * struct file_link_s - file linked list node
@@ -77,6 +41,39 @@ typedef struct file_link_s
 	struct file_link_s *next;
 	struct file_link_s *prev;
 } file_node_t;
+
+/**
+ * struct ls_config_s - flags struct
+ * @printer: printer (changed to a long printer if -l is present)
+ * @one_per_line: -1
+ * @dot: -a
+ * @dot_alt: -A ('.' and '..' ommitted)
+ * @reversed: -r
+ * @sort_by_size: -S
+ * @sort_by_time: -t
+ * @recursive: -R
+ * @print_dir_name: if true, print directory name before printing list
+ * @max_hard_links: max hard links
+ * @max_size: max size
+ * @max_strlen: max strlen
+ **/
+typedef struct ls_config_s
+{
+	void (*printer)(file_node_t *, struct ls_config_s *);
+	bool one_per_line;
+	bool dot;
+	bool dot_alt;
+	bool reversed;
+	bool sort_by_size;
+	bool sort_by_time;
+	bool recursive;
+	bool print_dir_name;
+	int max_hard_links;
+	int max_size;
+	int max_strlen;
+} ls_config_t;
+
+typedef void (*print_t)(file_node_t *, struct ls_config_s *);
 
 /**
  * struct dir_node_s - directory node
@@ -98,7 +95,6 @@ typedef struct dir_node_s
 #define ISLOWER(x) ((x) >= 'a' && (x) <= 'z')
 #define ISUPPER(x) ((x) >= 'A' && (x) <= 'Z')
 
-typedef void (*print_t)(file_node_t *, ls_config_t *);
 /* Function Prototypes */
 void get_permissions(char *buffer, mode_t mode);
 void get_group(char *buffer, gid_t group_id);
@@ -109,7 +105,7 @@ char *_strdup(char *str);
 char *_strcpy(char *dest, char *src);
 int add_dir_node(char *name, DIR *stream, dir_node_t **head);
 int add_file_node(char *file_name, char *dir_name, file_node_t **head);
-void set_flags(char *arg, ls_config_t *flags);
+int set_flags(char *arg, ls_config_t *flags);
 char *which_goes_first(char *s1, char *s2);
 int print_error_message(char *name);
 void print_list(file_node_t *file_list, ls_config_t *flags);
