e82d5049c45c71f6fa08789df7bcef8ec57f8f81
jicruz96 <accounts@jicruz.com>
2020-12-11 05:18:01 +0000

    change function names in sorters.c to be more descriptive

--- 0x00-ls/sorters.c
+++ 0x00-ls/sorters.c
@@ -1,11 +1,11 @@
 #include "header.h"
 
 /**
- * sort - sort
+ * sort_files_by_time - sort file nodes by time of most recent modification
  * @head: head
  * Return: head
  **/
-file_node_t *sort(file_node_t *head)
+file_node_t *sort_files_by_time(file_node_t *head)
 {
 	file_node_t *this = head;
 
@@ -14,29 +14,29 @@ file_node_t *sort(file_node_t *head)
 
 	while (this->next)
 	{
-		if (compare(this, this->next))
+		if (compare_files_by_time(this, this->next))
 		{
 			if (this == head)
 				head = this->next;
-			swap(this, this->next);
+			swap_files(this, this->next);
 		}
 		else
 			this = this->next;
 	}
 
 	for (this = head; this->next; this = this->next)
-		if (compare(this, this->next))
-			return (sort(head));
+		if (compare_files_by_time(this, this->next))
+			return (sort_files_by_time(head));
 
 	return (head);
 }
 
 /**
- * swap - swap
- * @a: a
- * @b: b
+ * swap_files - swap two file nodes
+ * @a: file node 1
+ * @b: file node 2
  **/
-void swap(file_node_t *a, file_node_t *b)
+void swap_files(file_node_t *a, file_node_t *b)
 {
 	if (a->prev)
 		a->prev->next = b;
@@ -49,12 +49,12 @@ void swap(file_node_t *a, file_node_t *b)
 }
 
 /**
- * compare - compare
- * @a: a
- * @b: b
+ * compare_files_by_time - compare file nodes by time of most recent mod
+ * @a: file node 1
+ * @b: file node 2
  * Return: 1 if a | 0 if b
  **/
-int compare(file_node_t *a, file_node_t *b)
+int compare_files_by_time(file_node_t *a, file_node_t *b)
 {
 	time_t a_time = a->info->st_mtime;
 	time_t b_time = b->info->st_mtime;
