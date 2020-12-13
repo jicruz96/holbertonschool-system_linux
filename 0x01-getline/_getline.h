9aa82120d1e5166d655d390f589252643146a8e9
jicruz96 <accounts@jicruz.com>
2020-12-13 23:52:15 +0000

    Organize header file

--- 0x01-getline/_getline.h
+++ 0x01-getline/_getline.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define READ_SIZE 10
+#define READ_SIZE 1024
 
 /**
  * struct reader_s -    associates a file descriptor to its file's contents.
@@ -15,8 +15,6 @@
  * @buf:		file's text
  * @bytes:	number of bytes copied into buffer (needed to differentiate
  *				between copied null bytes and null bytes from initialization)
- * @buf_size:	size of buffer allocated to memory
- * @i:			index where _getline left off reading the buffer
  * @next:		pointer to next reader struct
  **/
 typedef struct reader_s
@@ -24,16 +22,10 @@ typedef struct reader_s
 	int fd;
 	char *buf;
 	int bytes;
-	int buf_size;
-	int i;
 	struct reader_s *next;
 } reader_t;
 
 char *_getline(const int fd);
-char *return_line(char *line, reader_t *reader, int buf_len, int offset);
-char *free_readers(reader_t **readers);
-char *_memcpy(char *dest, char *src, size_t size);
-reader_t *reader_init(int fd);
 char *find_line(reader_t *rd);
 
 #endif /* GETLINE_H */
