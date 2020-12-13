b1f9e55263146103633bed3fa6b408fc43b818c8
jicruz96 <accounts@jicruz.com>
2020-12-13 03:41:55 +0000

    Revert to previous version of file

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -1,3 +1,4 @@
+
 #include "_getline.h"
 
 /**
@@ -10,17 +11,10 @@ char *_getline(const int fd)
 {
 	static reader_t *readers;
 	reader_t *rd;
-	ssize_t bytes_read;
-	char *bytes;
 
-	/* Free linked list of readers when fd = -1 is passed */
 	if (fd == -1)
-	{
-		for (; readers; readers = rd)
-			rd = readers->next, free(readers->buf), free(readers);
-		return (NULL);
-	}
-	/* Loop through known readers. If fd is found, find line */
+		return (free_readers(&readers));
+
 	for (rd = readers; rd; rd = rd->next)
 		if (rd->fd == fd)
 		{
@@ -28,24 +22,12 @@ char *_getline(const int fd)
 				rd->bytes = read(fd, rd->buf, READ_SIZE);
 			return (find_line(rd));
 		}
-	/* If not found, it's a new file. Create a new reader node */
-	bytes = malloc(sizeof(char) * READ_SIZE);
-	if (bytes == NULL)
-		return (NULL);
 
-	bytes_read = read(fd, bytes, READ_SIZE);
-	if (bytes_read <= 0)
-	{
-		free(bytes);
-		return (NULL);
-	}
-
-	rd = malloc(sizeof(reader_t));
+	rd = reader_init(fd);
 	if (rd == NULL)
 		return (NULL);
-	rd->fd = fd, rd->buf = bytes, rd->bytes = bytes_read;
-	rd->next = readers, readers = rd;
-
+	rd->next = readers;
+	readers = rd;
 	return (find_line(rd));
 }
 
@@ -56,43 +38,142 @@ char *_getline(const int fd)
  **/
 char *find_line(reader_t *rd)
 {
-	int i, j, line_size = 0, bytes_copied = 0;
+	int i, line_size = 0, bytes_copied = 0;
 	char *line = NULL, *tmp;
 
 	while (rd->bytes > 0)
 	{
 		if (line_size < bytes_copied + rd->bytes + 1)
 		{
-			line_size += rd->bytes + 1;
-			tmp = malloc(sizeof(char) * line_size);
+			line_size += rd->bytes + 1, tmp = malloc(sizeof(char) * line_size);
 			if (tmp == NULL)
 			{
 				free(line);
 				return (NULL);
 			}
-			memcpy(tmp, line, bytes_copied);
+			_memcpy(tmp, line, bytes_copied);
 			memset(tmp + bytes_copied, '\0', line_size - bytes_copied);
 			free(line), line = tmp;
 		}
 
 		for (i = 0; i < rd->bytes; i++)
+		{
 			if (rd->buf[i] == '\n')
 			{
-				rd->buf[i++] = '\0', rd->bytes -= i;
-				for (j = 0; j < rd->bytes; j++)
-				{
-					if (j < i)
-						(line + bytes_copied)[j] = rd->buf[j];
-					if (i + j < READ_SIZE)
-						rd->buf[j] = (rd->buf + i)[j], (rd->buf + i)[j] = '\0';
-				}
-				return (line);
+				rd->buf[i++] = '\0';
+				rd->bytes -= i;
+				return (return_line(line, rd, i, bytes_copied));
 			}
+		}
 
-		memcpy(line + bytes_copied, rd->buf, rd->bytes);
+		_memcpy(line + bytes_copied, rd->buf, rd->bytes);
 		bytes_copied += rd->bytes;
 		rd->bytes = read(rd->fd, rd->buf, READ_SIZE);
 	}
 
 	return (line);
+}
+
+/**
+ * return_line - merges two buffers, erases unneeded memory, returns buffer
+ * @line: line buffer
+ * @reader: reader node with buffer with extra chars and remaining byte count
+ * @buf_len: size of reader's buffer stuff
+ * @offset: what index of line to start copying extra into
+ * Return: line
+ **/
+char *return_line(char *line, reader_t *reader, int buf_len, int offset)
+{
+	int i;
+	char *next = reader->buf + buf_len;
+	char *line_end = line + offset;
+
+	for (i = 0; i < buf_len; i++)
+	{
+		line_end[i] = reader->buf[i];
+		if (i + buf_len < READ_SIZE)
+		{
+			reader->buf[i] = next[i];
+			next[i] = '\0';
+		}
+	}
+	for (; i < reader->bytes && (i + buf_len) < READ_SIZE; i++)
+	{
+		reader->buf[i] = next[i];
+		next[i] = '\0';
+	}
+
+	return (line);
+}
+
+/**
+ * reader_init - initializes new "reader" node. The reader associates a file
+ *				 descriptor to a buffer. _getline uses this buffer to parse
+ *				 through the lines of the file and read more file contents
+ *				 when necessary.
+ *
+ * @fd: file descriptor
+ * Return: pointer to new node
+ **/
+reader_t *reader_init(int fd)
+{
+	reader_t *new;
+	ssize_t bytes_read;
+	char *bytes = malloc(sizeof(char) * READ_SIZE);
+
+	bytes_read = read(fd, bytes, READ_SIZE);
+	if (bytes_read <= 0)
+	{
+		free(bytes);
+		return (NULL);
+	}
+	new = malloc(sizeof(reader_t));
+	new->fd = fd;
+	new->buf = bytes;
+	new->bytes = bytes_read;
+	new->i = 0;
+	new->next = NULL;
+	new->buf_size = READ_SIZE;
+	return (new);
+}
+
+/**
+ * free_readers - frees all reader nodes and node contents
+ * @readers: pointer to head of linked list of readers
+ * Return: NULL, always, which _getline immediately returns
+ **/
+char *free_readers(reader_t **readers)
+{
+	reader_t *curr, *tmp;
+
+	if (!readers || !(*readers))
+		return (NULL);
+
+	for (curr = *readers; curr; curr = tmp)
+	{
+		tmp = curr->next;
+		free(curr->buf);
+		free(curr);
+	}
+	*readers = NULL;
+	return (NULL);
+}
+
+/**
+ * _memcpy - custom memcpy. Normal memcpy raises an error if the two
+ *			 pointer areas overlap. This one does not.
+ *
+ * @dest: pointer to buffer where memory will be copies
+ * @src: source of content to be copied
+ * @size: number of bytes to copy over
+ * Return: pointer to dest
+ **/
+char *_memcpy(char *dest, char *src, size_t size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		dest[i] = src[i];
+
+	return (dest);
 }
