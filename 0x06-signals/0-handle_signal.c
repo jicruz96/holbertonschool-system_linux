b37e86ea3b0b58d68c3a804e29f0169ad4c2c9e8
J.I. Cruz <acounts@jicruz.com>
2021-03-03 13:59:23 -0500

    Initial commit of project folder 0x06 (along with first exercise)

---
+++ 0x06-signals/0-handle_signal.c
@@ -0,0 +1,50 @@
+#include "signals.h"
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * gotcha - prints "Gotcha! [<signum]" when called on by a signal handler
+ * @signum: signal number
+ **/
+void gotcha(int signum)
+{
+	char signum_str[3] = {'\0', '\0', '\0'};
+	char *gotcha_message = "Gotcha! ";
+
+	/**
+	 * This signum-to-signum_str section assumes there are fewer than 100
+	 * distinct signals (which, there are, so I'm good)
+	 **/
+	if (signum > 9)
+	{
+		signum_str[0] = (signum / 10) + '0';
+		signum_str[1] = (signum % 10) + '0';
+	}
+	else
+	{
+		signum_str[0] = signum + '0';
+	}
+
+	/**
+	 * "Why not just use printf?", I hear you ask.
+	 *
+	 * printf is not async-signal-safe and, therefore, it's bad practice to use
+	 * it in signal handlers. See `man 7 signal-safety` for a list of
+	 * async-signal-safe functions.
+	 **/
+	write(STDOUT_FILENO, gotcha_message, strlen(gotcha_message));
+	write(STDOUT_FILENO, "[", 1);
+	write(STDOUT_FILENO, signum_str, strlen(signum_str));
+	write(STDOUT_FILENO, "]\n", 2);
+}
+
+/**
+ * handle_signal - handles the SIGINT signal
+ * Return: 0 on success, -1 on error
+ **/
+int handle_signal(void)
+{
+	if (signal(SIGINT, gotcha) == SIG_ERR)
+		return (FAILURE);
+	return (SUCCESS);
+}
---
+++ 0x06-signals/README.md
@@ -0,0 +1,12 @@
+# 0x04 C - Signals
+
+This is the sixth project of the Holberton School Linux System section of the Low Level Systems, Algorithms, and Blockchain curriculum.
+
+## What we did 🤔
+
+In this project, we:
+* learned about signals and their use cases
+* built a series of functions that put these fucntions to the test
+
+
+> Written by J. to the is I.
