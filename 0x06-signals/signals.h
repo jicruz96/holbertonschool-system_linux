c88adf458f45461a196267535698314a1b6be3da
J.I. Cruz <acounts@jicruz.com>
2021-03-03 14:48:04 -0500

    update header

--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -3,10 +3,20 @@
 
 #include <signal.h>
 
+#ifndef sig_t
+typedef void (*sig_t)(int);
+#endif /* sig_t */
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif /* NULL */
+
 #define SUCCESS 0
 #define FAILURE -1
 
 
+
 int handle_signal(void);
+void (*current_handler_signal(void))(int);
 
 #endif /* SIGNALS_H */
