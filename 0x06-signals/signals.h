9c3cc3534d1c5fb1767fe7e64c3d1858b4ad71ef
J.I. Cruz <acounts@jicruz.com>
2021-03-08 11:29:12 -0500

    Update header; push file 2 to repo

--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -15,8 +15,35 @@ typedef void (*sig_t)(int);
 #define FAILURE -1
 
 
-
+/* 0-handle_signal.c */
 int handle_signal(void);
+
+/* 1-current_handler_signal.c */
 void (*current_handler_signal(void))(int);
+
+/* 2-handle_sigaction.c */
+int handle_sigaction(void);
+
+/* 3-current_handler_sigaction.c */
+void (*current_handler_sigaction(void))(int);
+
+/* 4-trace_signal_sender.c */
+int trace_signal_sender(void);
+
+/* 100-all_in_one.c */
+void all_in_one(void);
+
+/* 101-sigset_init.c */
+int sigset_init(sigset_t *set, int *signals);
+
+/* 102-signals_block.c */
+int signals_block(int *signals);
+
+/* 103-signals_unblock.c */
+int signals_unblock(int *signals);
+
+/* 104-handle_pending.c */
+int handle_pending(void (*handler)(int));
+
 
 #endif /* SIGNALS_H */
---
+++ 0x06-signals/test_files/1-main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "signals.h"
+
+/* Our functions */
+void gotcha(int);
+int handle_signal(void);
+
+/**
+ * main - Entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+	void (*handler)(int);
+	int i;
+
+	handler = current_handler_signal();
+	printf("Address of the current handler: %#lx\n", (unsigned long int)handler);
+
+	/* Set 'gotcha()` as the handler for SIGINT */
+	handle_signal();
+
+	handler = current_handler_signal();
+	printf("Address of the 'gotcha' function: %#lx\n", (unsigned long int)&gotcha);
+	printf("Address of the current handler: %#lx\n", (unsigned long int)handler);
+
+	for (i = 0; ; i++)
+	{
+		printf("[%d] Wait for it ...\n", i);
+		sleep(1);
+	}
+	return (EXIT_SUCCESS);
+}
