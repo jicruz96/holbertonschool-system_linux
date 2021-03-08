cabeeb41393f474bd58b43be115aa16bc43c84b0
J.I. Cruz <acounts@jicruz.com>
2021-03-08 13:14:20 -0500

    Push exercise 5, first attempt

---
+++ 0x06-signals/5-signal_describe.c
@@ -0,0 +1,28 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: EXIT_SUCCESS on success | EXIT_FAILURE on failure
+ **/
+int main(int argc, char *argv[])
+{
+	unsigned long int signum;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <signum>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	signum = atoi(argv[1]);
+
+	if (signum > (sizeof(sys_signame) / sizeof(char *)) - 1)
+		printf("%lu: Unknown signal %lu\n", signum, signum);
+	else
+		printf("%lu: %s\n", signum, sys_signame[signum]);
+
+	return (EXIT_SUCCESS);
+}
