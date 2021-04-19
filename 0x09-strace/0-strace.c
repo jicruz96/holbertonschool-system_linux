610b852161eebfca41406604b887773b8ea45fc0
jicruz96 <accounts@jicruz.com>
2021-04-19 07:17:17 +0000

    Minor tweaks to 0x09/0-strace.c: set stdout buf to NULL, init print to 0

--- 0x09-strace/0-strace.c
+++ 0x09-strace/0-strace.c
@@ -19,6 +19,7 @@ int main(int argc, char *argv[], char *envp[])
 		return (1);
 	}
 
+	setbuf(stdout, NULL);
 	pid = fork();
 
 	if (pid == 0)
@@ -28,13 +29,13 @@ int main(int argc, char *argv[], char *envp[])
 	}
 	else
 	{
-		for (status = 1, print = 1; !WIFEXITED(status); print ^= 1)
+		for (status = 1, print = 0; !WIFEXITED(status); print ^= 1)
 		{
 			ptrace(PT_SYSCALL, pid, NULL, NULL);
 			wait(&status);
 			ptrace(PT_GETREGS, pid, NULL, &regs);
 			if (print)
-				printf("%lu\n", (unsigned long int)regs.orig_rax);
+				printf("%lu\n", (size_t)regs.orig_rax);
 		}
 	}
 
