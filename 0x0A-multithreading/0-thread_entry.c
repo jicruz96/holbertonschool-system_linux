14e05108d95a1fe209ad8082951aa3c94cd1183b
J.I. Cruz <acounts@jicruz.com>
2021-05-17 01:03:45 -0500

    First go at task 0 for project 0x0A

--- 0x0A-multithreading/0-thread_entry.c
+++ 0x0A-multithreading/0-thread_entry.c
@@ -1,4 +1,7 @@
 #include "multithreading.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
  * thread_entry - entry point to a new thread
@@ -7,6 +10,9 @@
  **/
 void *thread_entry(void *arg)
 {
-	(void)arg;
+	if (arg)
+		write(STDOUT_FILENO, arg, strlen((char *)arg));
+	write(STDOUT_FILENO, "\n", 1);
+	pthread_exit(NULL);
 	return (NULL);
 }
