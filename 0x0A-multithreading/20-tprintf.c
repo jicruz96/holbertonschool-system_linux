197e44397cef6cfa2e947015f24a14c4fed48972
J.I. Cruz <acounts@jicruz.com>
2021-05-18 15:37:40 -0500

    First go at task 4 for project 0x0A

--- 0x0A-multithreading/20-tprintf.c
+++ 0x0A-multithreading/20-tprintf.c
@@ -1,13 +1,33 @@
 #include "multithreading.h"
+#include <stdio.h>
+#include <pthread.h>
+
+pthread_mutex_t lock;
 
 /**
- * trpintf - uses printf family to print out a given formatted string
+ * tprintf - uses printf family to print out a given formatted string
  *			 uses mutex to prevent race conditions
  * @format: formatted string
  * Return: number of characters printed
  **/
 int tprintf(char const *format, ...)
 {
-	(void)format;
+	pthread_t self = pthread_self();
+
+	pthread_mutex_lock(&lock);
+	printf("[%lu] %s", (unsigned long)self, format);
+	pthread_mutex_unlock(&lock);
 	return (0);
 }
+
+__attribute__((constructor)) void begin(void)
+{
+	pthread_mutex_init(&lock, NULL);
+}
+
+__attribute__((destructor)) void end(void)
+{
+	pthread_mutex_destroy(&lock);
+}
+
+void end(void) __attribute__((destructor));
