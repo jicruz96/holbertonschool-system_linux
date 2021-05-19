292e52dd205a0238384ee4c6d4c9c291d681c291
jicruz96 <accounts@jicruz.com>
2021-05-19 12:08:17 -0500

    Update list.h

--- 0x0A-multithreading/list.h
+++ 0x0A-multithreading/list.h
@@ -2,6 +2,7 @@
 #define LIST_H
 
 #include <stddef.h>
+#include <pthread.h>
 
 /**
  * struct node_s - Node structure
@@ -30,6 +31,42 @@ typedef struct list_s
 	node_t	*tail;
 	size_t	size;
 } list_t;
+
+typedef void *(*task_entry_t)(void *);
+
+/**
+ * enum task_status_e - Task statuses
+ *
+ * @PENDING: Task is pending
+ * @STARTED: Task has been started
+ * @SUCCESS: Task has completed successfully
+ * @FAILURE: Task has completed with issues
+ */
+typedef enum task_status_e
+{
+    PENDING = 0,
+    STARTED,
+    SUCCESS,
+    FAILURE
+} task_status_t;
+
+/**
+ * struct task_s - Executable task structure
+ *
+ * @entry:  Pointer to a function to serve as the task entry
+ * @param:  Address to a custom content to be passed to the entry function
+ * @status: Task status, default to PENDING
+ * @result: Stores the return value of the entry function
+ * @lock:   Task mutex
+ */
+typedef struct task_s
+{
+    task_entry_t    entry;
+    void        *param;
+    task_status_t   status;
+    void        *result;
+    pthread_mutex_t lock;
+} task_t;
 
 typedef void (*node_func_t)(void *);
 
