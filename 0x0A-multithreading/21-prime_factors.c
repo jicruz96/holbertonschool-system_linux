607d48d5466addb0bb6e345bc6d0465c1a6fb814
jicruz96 <accounts@jicruz.com>
2021-05-19 12:04:53 -0500

    First go at task 5 for project 0x0A

--- 0x0A-multithreading/21-prime_factors.c
+++ 0x0A-multithreading/21-prime_factors.c
@@ -1,4 +1,7 @@
 #include "multithreading.h"
+#include "list.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * prime_factors - factors a number into a list of prime factors
@@ -7,6 +10,29 @@
  **/
 list_t *prime_factors(char const *s)
 {
-	(void)s;
-	return (NULL);
+	unsigned long n = strtoul(s, NULL, 10);
+	unsigned long *tmp, p = 2;
+	list_t *list = malloc(sizeof(list_t));
+
+	list_init(list);
+	while (p <= n / 2)
+	{
+		while (n % p == 0)
+		{
+			tmp = malloc(sizeof(unsigned long));
+			*tmp = p;
+			list_add(list, (void *)tmp);
+			n /= p;
+		}
+
+		p += 1 + (p != 2);
+	}
+
+	if (n >= 2)
+	{
+		tmp = malloc(sizeof(unsigned long));
+		*tmp = n;
+		list_add(list, (void *)tmp);
+	}
+	return (list);
 }
