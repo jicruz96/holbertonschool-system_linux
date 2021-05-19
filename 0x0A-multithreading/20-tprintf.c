064562f2ca8a9f4ba6631a586518a1883d9db8b4
jicruz96 <accounts@jicruz.com>
2021-05-19 14:29:36 -0500

    Refactor 20-tprintf.c

--- 0x0A-multithreading/20-tprintf.c
+++ 0x0A-multithreading/20-tprintf.c
@@ -4,7 +4,7 @@
 #include <pthread.h>
 #include <stdarg.h>
 
-pthread_mutex_t lock;
+pthread_mutex_t tprintf_mutex;
 
 /**
  * tprintf - uses printf family to print out a given formatted string
@@ -16,24 +16,25 @@ int tprintf(char const *format, ...)
 {
 	pthread_t self = pthread_self();
 	va_list args;
+	int chars_printed;
 
 	va_start(args, format);
-	pthread_mutex_lock(&lock);
-	printf("[%lu] ", (unsigned long)self);
-	vprintf(format, args);
-	pthread_mutex_unlock(&lock);
+	pthread_mutex_lock(&tprintf_mutex);
+	chars_printed = printf("[%lu] ", (unsigned long)self);
+	chars_printed += vprintf(format, args);
+	pthread_mutex_unlock(&tprintf_mutex);
 	va_end(args);
-	return (0);
+	return (chars_printed);
 }
 
-__attribute__((constructor)) void begin(void)
+__attribute__((constructor)) void tprintf_mutex_init(void)
 {
-	pthread_mutex_init(&lock, NULL);
+	pthread_mutex_init(&tprintf_mutex, NULL);
 }
 
-__attribute__((destructor)) void end(void)
+__attribute__((destructor)) void tprintf_mutex_destroy(void)
 {
-	pthread_mutex_destroy(&lock);
+	pthread_mutex_destroy(&tprintf_mutex);
 }
 
 void end(void) __attribute__((destructor));
