da0dc18a3a4c33956d9fd5f7740222bdb7925b20
jicruz96 <accounts@jicruz.com>
2021-05-19 12:16:09 -0500

    update header files

--- 0x0A-multithreading/list.h
+++ 0x0A-multithreading/list.h
@@ -32,42 +32,6 @@ typedef struct list_s
 	size_t	size;
 } list_t;
 
-typedef void *(*task_entry_t)(void *);
-
-/**
- * enum task_status_e - Task statuses
- *
- * @PENDING: Task is pending
- * @STARTED: Task has been started
- * @SUCCESS: Task has completed successfully
- * @FAILURE: Task has completed with issues
- */
-typedef enum task_status_e
-{
-    PENDING = 0,
-    STARTED,
-    SUCCESS,
-    FAILURE
-} task_status_t;
-
-/**
- * struct task_s - Executable task structure
- *
- * @entry:  Pointer to a function to serve as the task entry
- * @param:  Address to a custom content to be passed to the entry function
- * @status: Task status, default to PENDING
- * @result: Stores the return value of the entry function
- * @lock:   Task mutex
- */
-typedef struct task_s
-{
-    task_entry_t    entry;
-    void        *param;
-    task_status_t   status;
-    void        *result;
-    pthread_mutex_t lock;
-} task_t;
-
 typedef void (*node_func_t)(void *);
 
 /* list.c */
--- 0x0A-multithreading/multithreading.h
+++ 0x0A-multithreading/multithreading.h
@@ -66,6 +66,42 @@ typedef struct blur_portion_s
 	kernel_t const *kernel;
 } blur_portion_t;
 
+typedef void *(*task_entry_t)(void *);
+
+/**
+ * enum task_status_e - Task statuses
+ *
+ * @PENDING: Task is pending
+ * @STARTED: Task has been started
+ * @SUCCESS: Task has completed successfully
+ * @FAILURE: Task has completed with issues
+ */
+typedef enum task_status_e
+{
+    PENDING = 0,
+    STARTED,
+    SUCCESS,
+    FAILURE
+} task_status_t;
+
+/**
+ * struct task_s - Executable task structure
+ *
+ * @entry:  Pointer to a function to serve as the task entry
+ * @param:  Address to a custom content to be passed to the entry function
+ * @status: Task status, default to PENDING
+ * @result: Stores the return value of the entry function
+ * @lock:   Task mutex
+ */
+typedef struct task_s
+{
+    task_entry_t    entry;
+    void        *param;
+    task_status_t   status;
+    void        *result;
+    pthread_mutex_t lock;
+} task_t;
+
 /* FUNCTION PROTOTYPES */
 
 /* 0-thread_entry.c */
@@ -93,6 +129,8 @@ task_t *create_task(task_entry_t entry, void *param);
 void destroy_task(task_t *task);
 void *exec_tasks(list_t const *tasks);
 list_t *prime_factors(char const *s);
+
+
 
 
 #endif /* MULTITHREADING_H */
