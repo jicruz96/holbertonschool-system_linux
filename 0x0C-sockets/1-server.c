2843db918b6e6e772637d8ef330edb45a45466e5
jicruz96 <accounts@jicruz.com>
2021-07-01 21:02:07 -0500

    First commit of file for Exercise 1

---
+++ 0x0C-sockets/1-server.c
@@ -0,0 +1,52 @@
+#include "sockets.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <errno.h>
+
+/**
+ * main - Opens an IPv4/TCP socket and listens to traffic on port 12345.
+ *        This is a learning tool only. The program can accept an entering
+ *        connection, and nothing else.
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int sockid = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	struct sockaddr_in addrport;
+	struct sockaddr client_addr;
+	socklen_t client_addr_size = sizeof(struct sockaddr);
+	int s;
+	unsigned int addr;
+
+
+	addrport.sin_family = AF_INET;
+	addrport.sin_port = htons(PORT);
+	addrport.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+	if (bind(sockid, (struct sockaddr *)&addrport, sizeof(addrport)) == -1)
+	{
+		perror("Bind:");
+		return (EXIT_FAILURE);
+	}
+
+	if (listen(sockid, 1) == -1)
+	{
+		perror("Listen:");
+		return (EXIT_FAILURE);
+	}
+
+	printf("Server listening on port %d\n", PORT);
+	s = accept(sockid, &client_addr, &client_addr_size);
+	addr = ((struct sockaddr_in *)&client_addr)->sin_addr.s_addr;
+	printf("Client connected: %u\n", addr);
+
+	close(sockid);
+	return (0);
+}
