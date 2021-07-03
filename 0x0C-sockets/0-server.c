6ced58e7f71bc655d0a1aebfb6d5dd016230a756
jicruz96 <accounts@jicruz.com>
2021-07-03 15:07:15 -0500

    Remove redundant headers from 0-server.c

--- 0x0C-sockets/0-server.c
+++ 0x0C-sockets/0-server.c
@@ -1,9 +1,7 @@
 #include "sockets.h"
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <errno.h>
