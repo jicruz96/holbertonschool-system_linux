6c5ad3f7e290074a737951a2f8b87f89caa97b49
jicruz96 <accounts@jicruz.com>
2021-08-14 13:05:44 -0400

    Refactor 2-client.c

--- 0x0C-sockets/2-client.c
+++ 0x0C-sockets/2-client.c
@@ -1,9 +1,30 @@
-#include "sockets.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include <arpa/inet.h>
 #include <netdb.h>
+#include <stdio.h>
 #include <unistd.h>
 
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+/**
+ * hints_init - defines the hints addrinfo struct, which we use for getaddrinfo
+ *
+ * @hints: pointer to addrinfo struct
+ */
+static void hints_init(struct addrinfo *hints)
+{
+	if (hints)
+	{
+		hints->ai_flags     = 0;
+		hints->ai_family    = AF_INET;
+		hints->ai_socktype  = SOCK_STREAM;
+		hints->ai_protocol  = IPPROTO_TCP;
+		hints->ai_addrlen   = 0;
+		hints->ai_addr      = NULL;
+		hints->ai_canonname = NULL;
+		hints->ai_next      = NULL;
+	}
+}
 
 /**
  * main - connects to a listening server. Takes 2 arguments:
@@ -14,8 +35,7 @@
  */
 int main(int argc, char *argv[])
 {
-	struct addrinfo hints = {0, AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, 0, 0};
-	struct addrinfo *host_ai, *tmp;
+	struct addrinfo hints, *host_addrinfo, *tmp;
 	int status, client_id;
 
 
@@ -25,7 +45,8 @@ int main(int argc, char *argv[])
 		return (EXIT_FAILURE);
 	}
 
-	status = getaddrinfo(argv[1], argv[2], &hints, &host_ai);
+	hints_init(&hints);
+	status = getaddrinfo(argv[1], argv[2], &hints, &host_addrinfo);
 	if (status)
 	{
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
@@ -39,18 +60,19 @@ int main(int argc, char *argv[])
 		return (EXIT_FAILURE);
 	}
 
-	for (tmp = host_ai; tmp; tmp = tmp->ai_next)
+	for (tmp = host_addrinfo; tmp; tmp = tmp->ai_next)
+	{
 		if (connect(client_id, tmp->ai_addr, tmp->ai_addrlen) == 0)
 		{
 			printf("Connected to %s:%s\n", argv[1], argv[2]);
-			freeaddrinfo(host_ai);
+			freeaddrinfo(host_addrinfo);
 			close(client_id);
 			return (EXIT_SUCCESS);
 		}
-
+	}
 
 	fprintf(stderr, "No valid address found for %s:%s\n", argv[1], argv[2]);
-	freeaddrinfo(host_ai);
+	freeaddrinfo(host_addrinfo);
 	close(client_id);
 	return (EXIT_FAILURE);
 }
