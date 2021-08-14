0f84c6dfbe3ad328c308179118fdf0589c6ab57d
jicruz96 <accounts@jicruz.com>
2021-08-14 13:37:09 -0400

    Rename 6-todo_api.c to 6-make_response.c and refactor

---
+++ 0x0C-sockets/6-make_response.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_headers - helper for take_requests()
+ * @buffer: buffer containing client request
+ */
+static void print_headers(char *buffer)
+{
+	char *headers = strchr(buffer, '\n') + 1;
+	char *header, *value;
+
+	header = strtok(headers, ":");
+	value = strtok(NULL, "\r");
+
+	while (header && value)
+	{
+		printf("Header: \"%s\" -> \"%s\"\n", header, ++value);
+		header = strtok(NULL, ":") + 1;
+		value = strtok(NULL, "\r");
+	}
+
+}
+
+/**
+ * make_response - creates response to a request
+ *
+ * @address: adress to respond to (only needed for info printing purposes)
+ * @request: buffer containing request from address
+ * Return: response
+ */
+char *make_response(char *address, char *request)
+{
+	printf("Client connected: %s\n", address);
+	printf("Raw request: \"%s\"\n", request);
+	print_headers(request);
+	fflush(stdout);
+	return (strdup("HTTP/1.1 200 OK\r\n\r\n"));
+}
